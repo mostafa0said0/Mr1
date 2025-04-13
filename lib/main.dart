6032950af5cdcbc268dbe8a1295959e28718029d
Mostafa Said Abdelwahab <mostafa.said.abdelwahab@outlook.com>
2025-04-13 13:20:12 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'عرض الصورة',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('عرض any.png'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image.asset(
+            'assets/any.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+    );
+  }
+}

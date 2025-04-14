2426a759a929d13c2b45ce2271e6d417c0caa2a2
Mostafa Said Abdelwahab <mostafa.said.abdelwahab@outlook.com>
2025-04-14 13:31:20 +0200

    fix2d

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -99,7 +99,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "61e88dd11af25d8d4eb7f8c429f4d4c6
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "cd8ef2a1949a94a4f0766d72a56a7d5b",
+"flutter_bootstrap.js": "fa883f07f235d428df2971f1fdad6177",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",

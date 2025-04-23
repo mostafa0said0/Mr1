50b51404b22ee197270868747a5bcaa018fc4dd4
Mostafa Said Abdelwahab <mostafa.said.abdelwahab@outlook.com>
2025-04-23 02:22:37 +0200

    نهائيي

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"010c8a806bccad64ab9972286b85dd24ca9844
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1958683550"
+    serviceWorkerVersion: "4225374369"
   }
 });

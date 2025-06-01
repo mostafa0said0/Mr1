ec61c880db57521e7294e15f103256db166ee0c3
Mostafa Said <mostafa.said.abdelwahab@outlook.com>
2025-06-01 08:02:14 +0300

    اضافه تحليل بياناتا

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"010c8a806bccad64ab9972286b85dd24ca98441f","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513fa","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "137304934"
+    serviceWorkerVersion: "2888720484"
   }
 });

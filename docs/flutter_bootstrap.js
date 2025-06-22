7ddfd004ad4681ba84cd0582dfc00aa2672cca84
mostafa0said0 <mostafa.said.abdelwahab@gmail.com>
2025-06-22 04:45:49 +0300

    e7

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1365001913"
+    serviceWorkerVersion: "3762537793"
   }
 });
